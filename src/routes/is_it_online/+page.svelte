2c79e20136ac9273edf5696d6803a3057c52f8bc
Damjan <damjandjordjevic1994@gmail.com>
2022-10-26 22:17:28 +0200

    added the 'is it online' feature

---
+++ src/routes/is_it_online/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { enhance } from '$app/forms';
+
+    export let form;
+
+    $: message = getMessage(form);
+     
+    function getMessage(state) {
+        let tempMessage = "";
+        switch(state) {
+            case true: {
+                tempMessage = "The site is online";
+                break;
+            }
+            case false: {
+                tempMessage = "The site is offline";
+                break;
+            }
+            default: {
+                tempMessage = "Enter a site to check";
+            }
+        }
+        return tempMessage;
+    }
+</script>
+
+<div>
+    <form method="post" use:enhance>
+        <input name="url"/>
+        <button type="submit">Check if online</button>
+    </form>
+    <p>{message}</p>
+</div>
