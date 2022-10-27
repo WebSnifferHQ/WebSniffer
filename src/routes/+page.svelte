2cd9cb97bff14489e739cdb49ff37460d37bf271
Karen Tamrazyan <karen.tamrazyan@gmail.com>
2022-10-27 23:21:13 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<a href="/whois">Who is</a>
-<a href="/is_it_online">Is it online</a>
+<h1>WebSniffer</h1>
+
+<ul>
+<li><a href="/whois">WHOIS lookup</a></li>
+<li><a href="/is_it_online">Is it online</a></li>
+</ul>
