1e977d99d85e72cc163aab8f2fb1b95ef2e6ea26
Karen Tamrazyan <karen.tamrazyan@gmail.com>
2022-10-30 03:45:07 +0100

    Update +page.svelte

--- src/routes/whois/+page.svelte
+++ src/routes/whois/+page.svelte
@@ -29,7 +29,9 @@
         {#each data as singleWhoisServer}
             <h2>WHOIS server: {singleWhoisServer.server}</h2>
             {#each Object.entries(singleWhoisServer.data) as item}
+                <div style="background-color:#dcdcdc;border:1px solid grey;padding:5px;border-radius:5px;overflow:auto;">
                 <pre>{item}</pre>
+                </div>
             {/each}
         {/each}
     {/if}
