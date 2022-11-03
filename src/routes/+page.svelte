5951d1634473614750ddb2a8d3cc5a97e0dd3fb4
Karen Tamrazyan <karen.tamrazyan@gmail.com>
2022-11-03 10:53:35 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
 
 <ul>
 <li><a href="/lookup/whois">WHOIS lookup</a></li>
-<li><a href="/lookup/is_it_online">Is it online</a></li>
-<li><a href="/lookup/domain_age">Domain age</a></li>
+<li><a href="/lookup/is-it-online">Is it online</a></li>
+<li><a href="/lookup/domain-age">Domain age</a></li>
 </ul>
