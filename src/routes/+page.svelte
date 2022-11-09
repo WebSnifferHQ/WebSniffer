5b7728e244401faee9cfa2090aff541cd84d9988
Karen Tamrazyan <karen.tamrazyan@gmail.com>
2022-11-09 23:38:53 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,17 +9,17 @@
   <Grid justify="center" align="center">
     <Grid.Col span={4}>
       <Card override={{ backgroundColor: '$blue300', display: 'flex', justifyContent:'center', margin:'1rem' }}>
-        <a href="/lookup/whois">WHOIS lookup</a>
+        <a href="/whois">WHOIS lookup</a>
       </Card>
     </Grid.Col>
     <Grid.Col span={4}>
       <Card override={{ backgroundColor: '$blue300', display: 'flex', justifyContent:'center', margin:'1rem' }}>
-        <a href="/lookup/is-it-online">Is it online</a>
+        <a href="/is-it-online">Is it online</a>
       </Card>
     </Grid.Col>
     <Grid.Col span={4}>
       <Card override={{ backgroundColor: '$blue300', display: 'flex', justifyContent:'center', margin:'1rem' }}>
-        <a href="/lookup/domain-age">Domain age</a>
+        <a href="/domain-age">Domain age</a>
       </Card>
     </Grid.Col>
   </Grid>
