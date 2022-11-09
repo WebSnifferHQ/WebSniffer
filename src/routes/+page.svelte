1b586b5428f3308aa2bbb34068b26a1b8edf0155
Karen Tamrazyan <karen.tamrazyan@gmail.com>
2022-11-09 23:44:08 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,22 +4,27 @@
 
 <Container>
   <Center>
-    <h1>WebSniffer</h1>
+    <h1><a href="/" style="text-decoration: none;">WebSniffer</a></h1>
   </Center>
   <Grid justify="center" align="center">
-    <Grid.Col span={4}>
+    <Grid.Col span={3}>
       <Card override={{ backgroundColor: '$blue300', display: 'flex', justifyContent:'center', margin:'1rem' }}>
         <a href="/whois">WHOIS lookup</a>
       </Card>
     </Grid.Col>
-    <Grid.Col span={4}>
+    <Grid.Col span={3}>
       <Card override={{ backgroundColor: '$blue300', display: 'flex', justifyContent:'center', margin:'1rem' }}>
         <a href="/is-it-online">Is it online</a>
       </Card>
     </Grid.Col>
-    <Grid.Col span={4}>
+    <Grid.Col span={3}>
       <Card override={{ backgroundColor: '$blue300', display: 'flex', justifyContent:'center', margin:'1rem' }}>
         <a href="/domain-age">Domain age</a>
+      </Card>
+    </Grid.Col>
+    <Grid.Col span={3}>
+      <Card override={{ backgroundColor: '$blue300', display: 'flex', justifyContent:'center', margin:'1rem' }}>
+        <a href="/ip">IP lookup</a>
       </Card>
     </Grid.Col>
   </Grid>
